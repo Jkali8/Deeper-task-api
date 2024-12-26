757f5d6c0c166bb2b274a4a5a278f220132c5f38
jkali8 <jkali8301@gmail.com>
2024-12-26 13:49:23 +0200

    Backend initial setup

---
+++ backend/src/main/java/com/github/Jkali8/deeper_task_backend/DeeperTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.Jkali8.deeper_task_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeperTaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeeperTaskBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/Jkali8/deeper_task_backend/DeeperTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Jkali8.deeper_task_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeperTaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
