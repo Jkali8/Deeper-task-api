a8a97730b010c21ba3800f45ab1e16a2f3669d2c
jkali8 <jkali8301@gmail.com>
2025-01-02 18:25:56 +0200

    Remove unused imports

--- backend/src/main/java/com/github/Jkali8/deeper_task_backend/DeeperTaskBackendApplication.java
+++ backend/src/main/java/com/github/Jkali8/deeper_task_backend/DeeperTaskBackendApplication.java
@@ -2,7 +2,6 @@ package com.github.Jkali8.deeper_task_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class DeeperTaskBackendApplication {
